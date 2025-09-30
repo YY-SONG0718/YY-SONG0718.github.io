e3b1fb1ac284e8044b0de685726adc860277acb5
YY-SONG0718 <48168412+YY-SONG0718@users.noreply.github.com>
2025-09-30 14:37:41 +0000

    Deploying to gh-pages from @ YY-SONG0718/YY-SONG0718.github.io@28e00538728e249ae5a1c1957244fa533c752f6d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Getting to know my qualifications and experiences.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
