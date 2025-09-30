6e559557b602fa9b2e92171b9838f9ccc4e3b630
YY-SONG0718 <48168412+YY-SONG0718@users.noreply.github.com>
2025-09-30 13:27:38 +0000

    Deploying to gh-pages from @ YY-SONG0718/YY-SONG0718.github.io@1ec3fa1a28b1c33b2e9c410fd4880d6a1196c1b9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%79%6C%76%69%65.%79%75%79%61%6F.%73%6F%6E%67@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/YY-SONG0718", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/yuyao-song-b339b21b7", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0001-9738-9726", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,7 +536,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=yZnZyo8AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
